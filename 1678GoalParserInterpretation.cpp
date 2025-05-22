194ec35adb27636d11f5d0334dca5af7a7eb0c3e
Narayan-A-R <naratnaparkhe@gmail.com>
2025-05-22 14:05:09 +0530

    Hidden test case passed

--- 1678GoalParserInterpretation.cpp
+++ 1678GoalParserInterpretation.cpp
@@ -2,33 +2,33 @@
 
 using namespace std;
 
-class Solution {
-public:
-    string interpret(string command) {
-        string ans="",buffer="";
-        for (int i = 0; i < command.size(); i++)
-        {
-            buffer+=command[i];
-            if(buffer=="G") {
-                ans+="G";
-                buffer="";
+    class Solution {
+    public:
+        string interpret(string command) {
+            string ans="",buffer="";
+            for (int i = 0; i < command.size(); i++)
+            {
+                buffer+=command[i];
+                if(buffer=="G") {
+                    ans+="G";
+                    buffer="";
+                }
+                if(buffer=="()") {
+                    ans+="o";
+                    buffer="";
+                }
+                if(buffer=="(al)") {
+                    ans+="al";
+                    buffer="";
+                }
             }
-            if(buffer=="()") {
-                ans+="o";
-                buffer="";
-            }
-            if(buffer=="(al)") {
-                ans+="al";
-                buffer="";
-            }
+            return ans;
         }
-        return ans;
-    }
-};
+    };
 
 int main(){
     Solution s;
-    string str="G()(al)";
-    s.interpret(str);
+    string str="G()()()()(al)";
+    cout<<s.interpret(str)<<"\n";
     return 0;
 }
