4831e31aff1ea1f37396d69e5175018648873ed6
Narayan-A-R <naratnaparkhe@gmail.com>
2025-05-22 14:02:11 +0530

    Input

---
+++ 1678GoalParserInterpretation.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    string interpret(string command) {
+        string ans="";
+        return ans;
+    }
+};
+
+int main(){
+    Solution s;
+    string str="G()(al)";
+    s.interpret(str);
+    return 0;
+}
