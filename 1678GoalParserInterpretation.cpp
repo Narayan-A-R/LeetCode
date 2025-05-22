1d509d6f780a78972415001a1698d9dbfea8a2cc
Narayan-A-R <naratnaparkhe@gmail.com>
2025-05-22 14:02:44 +0530

    Buffer type parsing

--- 1678GoalParserInterpretation.cpp
+++ 1678GoalParserInterpretation.cpp
@@ -5,7 +5,23 @@ using namespace std;
 class Solution {
 public:
     string interpret(string command) {
-        string ans="";
+        string ans="",buffer="";
+        for (int i = 0; i < command.size(); i++)
+        {
+            buffer+=command[i];
+            if(buffer=="G") {
+                ans+="G";
+                buffer="";
+            }
+            if(buffer=="()") {
+                ans+="o";
+                buffer="";
+            }
+            if(buffer=="(al)") {
+                ans+="al";
+                buffer="";
+            }
+        }
         return ans;
     }
 };
