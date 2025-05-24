5e639aeb2a1282f7db9a47c3d8bea42f992380f5
Narayan-A-R <naratnaparkhe@gmail.com>
2025-05-24 17:54:26 +0530

    to do nlogn sol use iterative and lowerbound

--- 300LongestIncreasingSubsequence.cpp
+++ 300LongestIncreasingSubsequence.cpp
@@ -2,14 +2,9 @@
 
 using namespace std;
 
-
-const int N=1e6+10;
-int arr[N];
-
 class Solution {
 public:
     int* dp;
-
     int func(int i,vector<int> & nums){
         if(dp[i] != -1) return dp[i];
 
@@ -28,23 +23,23 @@ public:
         dp= new int[n];
         for(int i=0;i<n;i++) dp[i]=-1;
 
-        int maximum=-1;
+        int ans=-1;
         for (int i = 0; i < n; i++)
         {
-            maximum=max(maximum,func(i,nums));
+            if(dp[i]!=-1){
+                ans=max(ans,dp[i]);
+                continue;
+            }
+            ans=max(ans,func(i,nums));
         }
-        
-        return maximum;
+        return ans;
     }
 };
 
 
 int main(){
     Solution s;
-    vector<int> nums={10,9,2,5,3,7,101,18};
+    vector<int> nums={2,15,3,7,8,6,18};
     cout<<s.lengthOfLIS(nums)<<'\n';
     return 0;
 }
-
-// 8
-// 8 9 1 2 3 4 5 18
