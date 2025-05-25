f55e4d98b326ba90b1fe134c54e6a5fa6f212852
Narayan-A-R <naratnaparkhe@gmail.com>
2025-05-25 21:13:51 +0530

    Saw online solution , vector unnesssary , use INT_MAX, dp was correct

--- 322CoinChange.cpp
+++ 322CoinChange.cpp
@@ -4,47 +4,36 @@ using namespace std;
 
 class Solution {
 public:
-    int * dp;
+    int dp[10010];
 
-    int minCoinsRequred(vector<int>& coins, int amount,int usedCoins){
-        if(amount==0) return usedCoins;
-        int n=coins.size();
-        vector<int> noOfCoinsUsed;
-        for (int i = 0; i < n; i++)
+    int minCoinsRequred(vector<int>& coins, int amount){
+        
+        if(dp[amount]!=-1) return dp[amount];
+        if(amount==0) return 0;
+        int ans=INT_MAX;
+        for (int i = 0; i <coins.size(); i++)
         {
             if(amount>=coins[i]){
-                noOfCoinsUsed.push_back(minCoinsRequred(coins,amount-coins[i],usedCoins+1));
+                ans=min(ans+0LL,minCoinsRequred(coins,amount-coins[i])+1LL);
             }
         }
-        if(noOfCoinsUsed.empty()) return -1;
-        int min=noOfCoinsUsed[0];
-        for (int i = 1; i < noOfCoinsUsed.size(); i++)
-        {
-            if(noOfCoinsUsed[i]<min){
-                min=noOfCoinsUsed[i];
-            }
-        }
-        return min;
+        return dp[amount]=ans;
     }
-    int coinChange(vector<int>& coins, int amount,int usedCoins=0) {
-        dp= new int[amount+10];
-        for(int i=0;i<=amount;i++) dp[i]=-2;
-
-        
-        int ans=minCoinsRequred(coins,amount,0);
-        for (int i = 0; i <=amount; i++)
-        {
-            cout<<dp[i]<<" "<<i<<" i\n";
-        }
-        cout<<'\n';
-        return ans;
+    int coinChange(vector<int>& coins, int amount){
+        for(int i=0;i<=amount;i++) dp[i]=-1;
+        int ans=minCoinsRequred(coins,amount);
+        return ans==INT_MAX?-1:ans;
     }
 };
 
+
 int main(){
     Solution s;
-    vector<int> coins={1,2,5};
-    int amount=11;
+    vector<int> coins={186,419,83,408};
+    int amount=6249;
     cout<<s.coinChange(coins,amount)<<'\n';
     return 0;
 }
+
+// 186,419,83,408
+// 6249
