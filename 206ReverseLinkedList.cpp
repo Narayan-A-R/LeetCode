d42df438712598292b24f7004900daa2e867624b
Narayan-A-R <naratnaparkhe@gmail.com>
2025-07-18 14:36:21 +0530

    kinda slow, recursive approach exists

---
+++ 206ReverseLinkedList.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode * prev=nullptr;
+        ListNode * node= nullptr;
+        while (head)
+        {
+            node = new ListNode();
+            node->next=prev;
+            node->val=head->val;
+            prev =  node;
+            head=head->next;
+        }
+        return node;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
