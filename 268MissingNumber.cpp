ea26871e8f93328e795861499796b543cdcb5d83
Narayan-A-R <naratnaparkhe@gmail.com>
2025-07-26 09:32:39 +0530

    xor could have been used

---
+++ 268MissingNumber.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int sum =0;
+        for (int i = 0; i < n; i++) sum+=nums[i];
+        return n*(n+1)/2 - sum;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
