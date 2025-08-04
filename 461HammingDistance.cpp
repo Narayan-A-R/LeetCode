5462f971492a3fc27a0155dbeb79f2bffc5f925b
Narayan-A-R <naratnaparkhe@gmail.com>
2025-08-04 18:07:25 +0530

    xor of ith digit of both x and y

---
+++ 461HammingDistance.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int count=0;
+        while (x || y)
+        {
+            if((x&1)^(y&1)) count++;
+            x>>=1;
+            y>>=1;
+        }
+        return count;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
