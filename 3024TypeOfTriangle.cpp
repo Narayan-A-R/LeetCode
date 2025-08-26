f711f2e74e23b2aa8327a5a405db5628af7fd486
Narayan-A-R <naratnaparkhe@gmail.com>
2025-08-26 22:54:10 +0530

    logic based+ basic maths

---
+++ 3024TypeOfTriangle.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        int s1=nums[0];
+        int s2=nums[1];
+        int s3=nums[2];
+        if(s1+s2<=s3 || s2+s3<=s1 || s1+s3<=s2) return "none";
+        if(s1==s2 && s2==s3) return "equilateral";
+        if(s1!=s2 && s2!=s3 && s1!=s3) return "scalene";
+        return "isosceles "; 
+    }
+};
+
+int main(){
+    
+    return 0;
+}
