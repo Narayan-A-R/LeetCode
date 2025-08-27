c93cacfa735d25eecf7396d1e7c5b598a28a8c12
Narayan-A-R <naratnaparkhe@gmail.com>
2025-08-28 00:05:42 +0530

    use abs for coditions but store nums i

---
+++ 2239FindClosestNumberToZero.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int findClosestNumber(vector<int>& nums) {
+        int n=nums.size();
+        int closest = nums[0];
+        for (int i = 0; i < n; i++) if(abs(nums[i])<abs(closest) ||(abs(nums[i])==abs(closest) && nums[i]>closest)) closest = nums[i];
+        return closest;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
