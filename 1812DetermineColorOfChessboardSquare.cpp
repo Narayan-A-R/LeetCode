8eecdbc8f4d19749c052507692bf81de484c8e00
Narayan-A-R <naratnaparkhe@gmail.com>
2025-09-01 00:01:57 +0530

    simple odd even pattern

---
+++ 1812DetermineColorOfChessboardSquare.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool squareIsWhite(string &c) {
+        return (c[0]+c[1])&1;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
